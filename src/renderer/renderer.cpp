31fcdc2f2f3a271d704ae65a66b1e8af8082fd5c
furrymonster <4urrym0nster@gmail.com>
2025-12-05 22:26:10 +0800

    refactor: i have a good idea

--- src/renderer/renderer.cpp
+++
@@ -1,7 +0,0 @@
-#include "renderer.h"
-
-namespace Nano
-{
-    Renderer::Renderer(std::shared_ptr<Window> window) : m_window(window) {}
-
-} // namespace Nano
--- src/window/window.cpp
+++
@@ -1,9 +0,0 @@
-#include "window.h"
-
-namespace Nano
-{
-    Window::Window(const WindowConfig& config) :
-        m_width(config.width), m_height(config.height), m_title(config.title), m_resizable(config.resizable)
-    {}
-
-} // namespace Nano
